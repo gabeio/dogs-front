d4613c593e7285814981b0bd64d6222a71c458b3
Gabe De Luca <me@gabe.io>
2021-06-20 00:32:15 -0400

    changes to async.series

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const dogsBackend = {
 	url: "https://6fr0s1p5oc.execute-api.us-east-2.amazonaws.com/dogs",
 	jwt: "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InpGTE1kaUR3VW9rMEpKSU0zWnJWOSJ9.eyJpc3MiOiJodHRwczovL2dhYmVpby51cy5hdXRoMC5jb20vIiwic3ViIjoidDhMdXBZMUFwZW1jYk5QWGxUN1dub1BxSENqOXA3RnhAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vYXBpLmRvZ3MuZ2FiZS5pbyIsImlhdCI6MTYyNDE1MzA0NSwiZXhwIjoxNjI0MjM5NDQ1LCJhenAiOiJ0OEx1cFkxQXBlbWNiTlBYbFQ3V25vUHFIQ2o5cDdGeCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.l8Fg9dAC4PVK0QuAN-m5hJjOVoMdqBluiGVZR_v4K93jg8ETfKb__PqcQFtrT1_7PxCuExvpvYDnvPS7yFl1ceaTj5fbOnO3p_PII_dV_mJ3ZjirJbtHw1-__i8JfyBOeAEBcHYzw5dQbSItTJhZHIEkOZFWUs6_O-KxoL1i0OOcykk1eBj4g3TIXrPh0e4AWOn5-GlDC8UxFaDYqI1Rz7jsY9UcHT2BVCzq-2O9N4A9-TB3ToZxbcw2_r_nYmJ_likLAdjJko-IfR3akcpik1R6bNKDXagb-aMmj1IPh5EZbKj4vvfcer8aNtLkaadSAzXEpSBq_mugOrIvbBtyGw",
 	get: function (callback) {
-		//
-		const response = fetch(this.url, {
+		fetch(this.url, {
 			credentials: 'include',
 			headers: {
 				'authorization': this.jwt,
@@ -22,8 +21,7 @@ const dogsBackend = {
 	},
 	set: function (dog, callback) {
 		console.log(dog)
-		const response = fetch(this.url, {
-			//
+		fetch(this.url, {
 			method: 'POST',
 			credentials: 'include',
 			headers: {
@@ -49,12 +47,26 @@ const app = Vue.createApp({
 		outside: function (dog) {
 			console.log("outside", dog)
 			dog.value = true
-			dogsBackend.set(dog, dogsBackend.get())
+			async.series([
+				function (callback) {
+					dogsBackend.set(dog, callback)
+				},
+				function (callback) {
+					dogsBackend.get()
+				},
+			])
 		},
 		inside: function (dog) {
 			console.log("inside", dog)
 			dog.value = false
-			dogsBackend.set(dog, dogsBackend.get())
+			async.series([
+				function (callback) {
+					dogsBackend.set(dog, callback)
+				},
+				function (callback) {
+					dogsBackend.get()
+				},
+			])
 		},
 		update: function (dogs) {
 			for (dog of dogs) {
