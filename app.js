9b736bc66140cc72f1869272d2a41e72acd56cd4
Gabe De Luca <me@gabe.io>
2021-06-20 10:19:19 -0400

    moves auth0 inside app.js

--- app.js
+++ app.js
@@ -105,3 +105,128 @@ const vm = app.mount('.dogs')
 console.log("interval: ", setInterval(function() {
 	dogsBackend.get()
 }, 5000))
+
+
+// The Auth0 client, initialized in configureClient()
+let auth0 = null;
+
+/**
+ * Starts the authentication flow
+ */
+const login = async (targetUrl) => {
+  try {
+    console.log("Logging in", targetUrl);
+
+    const options = {
+      redirect_uri: window.location.origin
+    };
+
+    if (targetUrl) {
+      options.appState = { targetUrl };
+    }
+
+    await auth0.loginWithRedirect(options);
+  } catch (err) {
+    console.log("Log in failed", err);
+  }
+};
+
+/**
+ * Executes the logout flow
+ */
+const logout = () => {
+  try {
+    console.log("Logging out");
+    auth0.logout({
+      returnTo: window.location.origin
+    });
+  } catch (err) {
+    console.log("Log out failed", err);
+  }
+};
+
+/**
+ * Initializes the Auth0 client
+ */
+const configureClient = async () => {
+  auth0 = await createAuth0Client({
+    domain: "https://gabeio.us.auth0.com",
+    client_id: "t8LupY1ApemcbNPXlT7WnoPqHCj9p7Fx",
+    audience: "https://api.dogs.gabe.io",
+  });
+};
+
+/**
+ * Checks to see if the user is authenticated. If so, `fn` is executed. Otherwise, the user
+ * is prompted to log in
+ * @param {*} fn The function to execute if the user is logged in
+ */
+const requireAuth = async (fn, targetUrl) => {
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    return fn();
+  }
+
+  return login(targetUrl);
+};
+
+/**
+ * Calls the API endpoint with an authorization token
+ */
+const callApi = async () => {
+  try {
+    const token = await auth0.getTokenSilently();
+
+    const response = await fetch("/api/external", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    const responseData = await response.json();
+    const responseElement = document.getElementById("api-call-result");
+
+    responseElement.innerText = JSON.stringify(responseData, {}, 2);
+
+    document.querySelectorAll("pre code").forEach(hljs.highlightBlock);
+
+    eachElement(".result-block", (c) => c.classList.add("show"));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// Will run when page finishes loading
+window.onload = async () => {
+  await configureClient();
+
+  const bodyElement = document.getElementsByTagName("body")[0];
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    console.log("> User is authenticated");
+    window.history.replaceState({}, document.title, window.location.pathname);
+    updateUI();
+    return;
+  }
+
+  console.log("> User not authenticated");
+
+  const query = window.location.search;
+  const shouldParseResult = query.includes("code=") && query.includes("state=");
+
+  if (shouldParseResult) {
+    console.log("> Parsing redirect");
+    try {
+      const result = await auth0.handleRedirectCallback();
+
+      console.log("Logged in!");
+    } catch (err) {
+      console.log("Error parsing redirect:", err);
+    }
+
+    window.history.replaceState({}, document.title, "/");
+  }
+};
