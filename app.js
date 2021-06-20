5fad432979bcc4e912e3ae7110b1c35e4c900125
Gabe De Luca <me@gabe.io>
2021-06-20 10:38:13 -0400

    cleaning up auth0 code

--- app.js
+++ app.js
@@ -42,72 +42,16 @@ const logout = () => {
 	}
 };
 
-/**
- * Checks to see if the user is authenticated. If so, `fn` is executed. Otherwise, the user
- * is prompted to log in
- * @param {*} fn The function to execute if the user is logged in
- */
-const requireAuth = async (fn, targetUrl) => {
-	const isAuthenticated = await auth0.isAuthenticated();
-
-	if (isAuthenticated) {
-		return fn();
-	}
-
-	return login(targetUrl);
-};
-
-/**
- * Calls the API endpoint with an authorization token
- */
-const callApi = async () => {
-	try {
-		const token = await auth0.getTokenSilently();
-
-		const response = await fetch("/api/external", {
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		});
-
-		const responseData = await response.json();
-		const responseElement = document.getElementById("api-call-result");
-
-		responseElement.innerText = JSON.stringify(responseData, {}, 2);
-
-		document.querySelectorAll("pre code").forEach(hljs.highlightBlock);
-
-		eachElement(".result-block", (c) => c.classList.add("show"));
-	} catch (e) {
-		console.error(e);
-	}
-};
 
 // Will run when page finishes loading
 window.onload = async () => {
-	const bodyElement = document.getElementsByTagName("body")[0];
-
 	const isAuthenticated = await auth0.isAuthenticated();
 
 	if (isAuthenticated) {
 		console.log("> User is authenticated");
 		return;
-	}
-
-	console.log("> User not authenticated");
-
-	const query = window.location.search;
-	const shouldParseResult = query.includes("code=") && query.includes("state=");
-
-	if (shouldParseResult) {
-		console.log("> Parsing redirect");
-		try {
-			const result = await auth0.handleRedirectCallback();
-
-			console.log("Logged in!");
-		} catch (err) {
-			console.log("Error parsing redirect:", err);
-		}
+	} else {
+		console.log("> User not authenticated");
 	}
 };
 
@@ -121,51 +65,55 @@ const dogsBackend = {
 	url: "https://6fr0s1p5oc.execute-api.us-east-2.amazonaws.com/dogs",
 	jwt: "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InpGTE1kaUR3VW9rMEpKSU0zWnJWOSJ9.eyJpc3MiOiJodHRwczovL2dhYmVpby51cy5hdXRoMC5jb20vIiwic3ViIjoidDhMdXBZMUFwZW1jYk5QWGxUN1dub1BxSENqOXA3RnhAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vYXBpLmRvZ3MuZ2FiZS5pbyIsImlhdCI6MTYyNDE1MzA0NSwiZXhwIjoxNjI0MjM5NDQ1LCJhenAiOiJ0OEx1cFkxQXBlbWNiTlBYbFQ3V25vUHFIQ2o5cDdGeCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.l8Fg9dAC4PVK0QuAN-m5hJjOVoMdqBluiGVZR_v4K93jg8ETfKb__PqcQFtrT1_7PxCuExvpvYDnvPS7yFl1ceaTj5fbOnO3p_PII_dV_mJ3ZjirJbtHw1-__i8JfyBOeAEBcHYzw5dQbSItTJhZHIEkOZFWUs6_O-KxoL1i0OOcykk1eBj4g3TIXrPh0e4AWOn5-GlDC8UxFaDYqI1Rz7jsY9UcHT2BVCzq-2O9N4A9-TB3ToZxbcw2_r_nYmJ_likLAdjJko-IfR3akcpik1R6bNKDXagb-aMmj1IPh5EZbKj4vvfcer8aNtLkaadSAzXEpSBq_mugOrIvbBtyGw",
 	get: function (callback) {
-		if (auth0.isAuthenticated) {
-			fetch(this.url, {
-				credentials: 'include',
-				headers: {
-					'authorization': this.jwt,
-				},
-			})
-			.then(response => response.json())
-			.then(data => {
-				console.log('Success:', data);
-				if (vm && vm.dogs) {
-					vm.update(data)
-				} else {
-					vm.initial(data)
-				}
-			})
-			.then(function () {
-				if (isFunction(callback)) {
-					callback(null)
-				}
-			})
-		} else {
-			login("https://dogs.gabe.io")
-		}
+		auth0.isAuthenticated().then(authed => {
+			if (authed) {
+				fetch(this.url, {
+					credentials: 'include',
+					headers: {
+						'authorization': this.jwt,
+					},
+				})
+				.then(response => response.json())
+				.then(data => {
+					console.log('Success:', data);
+					if (vm && vm.dogs) {
+						vm.update(data)
+					} else {
+						vm.initial(data)
+					}
+				})
+				.then(function () {
+					if (isFunction(callback)) {
+						callback(null)
+					}
+				})
+			} else {
+				login("https://dogs.gabe.io")
+			}
+		})
 	},
 	set: function (dog, callback) {
-		if (auth0.isAuthenticated) {
-			console.log(dog)
-			fetch(this.url, {
-				method: 'POST',
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-					'authorization': this.jwt,
-				},
-				body: JSON.stringify(dog),
-			})
-			.then(function () {
-				if (isFunction(callback)) {
-					callback(null)
-				}
-			})
-		} else {
-			login("https://dogs.gabe.io")
-		}
+		auth0.isAuthenticated().then(authed => {
+			if (authed) {
+				console.log(dog)
+				fetch(this.url, {
+					method: 'POST',
+					credentials: 'include',
+					headers: {
+						'Content-Type': 'application/json',
+						'authorization': this.jwt,
+					},
+					body: JSON.stringify(dog),
+				})
+				.then(function () {
+					if (isFunction(callback)) {
+						callback(null)
+					}
+				})
+			} else {
+				login("https://dogs.gabe.io")
+			}
+		})
 	}
 }
 
