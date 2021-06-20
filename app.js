923820dda8640cf6e9639440842e0df8f479b799
Gabe De Luca <me@gabe.io>
2021-06-20 10:44:06 -0400

    still trying to get javascript promises to work

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // The Auth0 client, initialized in configureClient()
-let auth0 = createAuth0Client({
+let auth0 = null
+createAuth0Client({
 	domain: "https://gabeio.us.auth0.com",
 	client_id: "t8LupY1ApemcbNPXlT7WnoPqHCj9p7Fx",
 	audience: "https://api.dogs.gabe.io",
@@ -42,54 +43,37 @@ const logout = () => {
 	}
 };
 
-
-// Will run when page finishes loading
-window.onload = async () => {
-	const isAuthenticated = await auth0.isAuthenticated();
-
-	if (isAuthenticated) {
-		console.log("> User is authenticated");
-		return;
-	} else {
-		console.log("> User not authenticated");
-	}
-};
-
-
-
-
-
-
-
 const dogsBackend = {
 	url: "https://6fr0s1p5oc.execute-api.us-east-2.amazonaws.com/dogs",
 	jwt: "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InpGTE1kaUR3VW9rMEpKSU0zWnJWOSJ9.eyJpc3MiOiJodHRwczovL2dhYmVpby51cy5hdXRoMC5jb20vIiwic3ViIjoidDhMdXBZMUFwZW1jYk5QWGxUN1dub1BxSENqOXA3RnhAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vYXBpLmRvZ3MuZ2FiZS5pbyIsImlhdCI6MTYyNDE1MzA0NSwiZXhwIjoxNjI0MjM5NDQ1LCJhenAiOiJ0OEx1cFkxQXBlbWNiTlBYbFQ3V25vUHFIQ2o5cDdGeCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.l8Fg9dAC4PVK0QuAN-m5hJjOVoMdqBluiGVZR_v4K93jg8ETfKb__PqcQFtrT1_7PxCuExvpvYDnvPS7yFl1ceaTj5fbOnO3p_PII_dV_mJ3ZjirJbtHw1-__i8JfyBOeAEBcHYzw5dQbSItTJhZHIEkOZFWUs6_O-KxoL1i0OOcykk1eBj4g3TIXrPh0e4AWOn5-GlDC8UxFaDYqI1Rz7jsY9UcHT2BVCzq-2O9N4A9-TB3ToZxbcw2_r_nYmJ_likLAdjJko-IfR3akcpik1R6bNKDXagb-aMmj1IPh5EZbKj4vvfcer8aNtLkaadSAzXEpSBq_mugOrIvbBtyGw",
 	get: function (callback) {
-		auth0.isAuthenticated().then(authed => {
-			if (authed) {
-				fetch(this.url, {
-					credentials: 'include',
-					headers: {
-						'authorization': this.jwt,
-					},
-				})
-				.then(response => response.json())
-				.then(data => {
-					console.log('Success:', data);
-					if (vm && vm.dogs) {
-						vm.update(data)
-					} else {
-						vm.initial(data)
-					}
-				})
-				.then(function () {
-					if (isFunction(callback)) {
-						callback(null)
-					}
-				})
-			} else {
-				login("https://dogs.gabe.io")
-			}
+		auth0.then(auth0 => {
+			auth0.isAuthenticated().then(authed => {
+				if (authed) {
+					fetch(this.url, {
+						credentials: 'include',
+						headers: {
+							'authorization': this.jwt,
+						},
+					})
+					.then(response => response.json())
+					.then(data => {
+						console.log('Success:', data);
+						if (vm && vm.dogs) {
+							vm.update(data)
+						} else {
+							vm.initial(data)
+						}
+					})
+					.then(function () {
+						if (isFunction(callback)) {
+							callback(null)
+						}
+					})
+				} else {
+					login("https://dogs.gabe.io")
+				}
+			})
 		})
 	},
 	set: function (dog, callback) {
