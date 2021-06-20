56e5d5eb8d4d64afe1788abb0a62aeaf3853a5a9
Gabe De Luca <me@gabe.io>
2021-06-20 10:45:17 -0400

    changes null back to promise

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // The Auth0 client, initialized in configureClient()
-let auth0 = null
-createAuth0Client({
+let auth0 = createAuth0Client({
 	domain: "https://gabeio.us.auth0.com",
 	client_id: "t8LupY1ApemcbNPXlT7WnoPqHCj9p7Fx",
 	audience: "https://api.dogs.gabe.io",
