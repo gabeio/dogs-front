d616b306a764964cdd2d351eb4e543772a451bdf
Gabe De Luca <me@gabe.io>
2021-06-20 23:53:22 -0400

    deepening the indentation

--- app.js
+++ app.js
@@ -7,30 +7,61 @@ let auth0 = createAuth0Client({
 	client_id: "t8LupY1ApemcbNPXlT7WnoPqHCj9p7Fx",
 	audience: "https://api.dogs.gabe.io",
 }).then(auth0 => {
+	console.log("createAuth0Client: auth0", auth0)
+	console.log("shouldParseResult", shouldParseResult)
 	if (shouldParseResult) {
-		const result = await auth0.handleRedirectCallback();
-
-		const dogsBackend = {
-			url: "https://6fr0s1p5oc.execute-api.us-east-2.amazonaws.com/dogs",
-			jwt: "",
-			get: function (callback) {
-				auth0.then(auth0 => {
+		auth0.handleRedirectCallback().then(result => {
+			console.log("handleRedirectCallback: result", result)
+			const dogsBackend = {
+				url: "https://6fr0s1p5oc.execute-api.us-east-2.amazonaws.com/dogs",
+				jwt: "",
+				get: function (callback) {
+					auth0.then(auth0 => {
+						console.log("dogsBackend.get: auth0.then", auth0)
+						auth0.isAuthenticated().then(authed => {
+							console.log("dogsBackend.get: auth0.isAuthenticated", authed)
+							if (authed) {
+								fetch(this.url, {
+									credentials: 'include',
+									headers: {
+										'authorization': this.jwt,
+									},
+								})
+								.then(response => response.json())
+								.then(data => {
+									console.log('Success:', data);
+									if (vm && vm.dogs) {
+										vm.update(data)
+									} else {
+										vm.initial(data)
+									}
+								})
+								.then(function () {
+									if (isFunction(callback)) {
+										callback(null)
+									}
+								})
+							} else {
+								auth0.loginWithRedirect({
+									redirect_uri: window.location.origin
+								})
+							}
+						})
+					})
+				},
+				set: function (dog, callback) {
 					auth0.isAuthenticated().then(authed => {
+						console.log("dogsBackend.get: auth0.isAuthenticated", authed)
 						if (authed) {
+							console.log(dog)
 							fetch(this.url, {
+								method: 'POST',
 								credentials: 'include',
 								headers: {
+									'Content-Type': 'application/json',
 									'authorization': this.jwt,
 								},
-							})
-							.then(response => response.json())
-							.then(data => {
-								console.log('Success:', data);
-								if (vm && vm.dogs) {
-									vm.update(data)
-								} else {
-									vm.initial(data)
-								}
+								body: JSON.stringify(dog),
 							})
 							.then(function () {
 								if (isFunction(callback)) {
@@ -43,90 +74,65 @@ let auth0 = createAuth0Client({
 							})
 						}
 					})
-				})
-			},
-			set: function (dog, callback) {
-				auth0.isAuthenticated().then(authed => {
-					if (authed) {
-						console.log(dog)
-						fetch(this.url, {
-							method: 'POST',
-							credentials: 'include',
-							headers: {
-								'Content-Type': 'application/json',
-								'authorization': this.jwt,
-							},
-							body: JSON.stringify(dog),
-						})
-						.then(function () {
-							if (isFunction(callback)) {
-								callback(null)
-							}
-						})
-					} else {
-						auth0.loginWithRedirect({
-							redirect_uri: window.location.origin
-						})
-					}
-				})
-			}
-		}
-		
-		function isFunction(functionToCheck) {
-			return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
-		}
-		
-		const app = Vue.createApp({
-			data() {
-				return {
-					dogs: dogsBackend.get(),
 				}
-			},
-			methods: {
-				initial: function (dogs) {
-					this.dogs = dogs
+			}
+			
+			function isFunction(functionToCheck) {
+				return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+			}
+			
+			const app = Vue.createApp({
+				data() {
+					return {
+						dogs: dogsBackend.get(),
+					}
 				},
-				outside: function (dog) {
-					console.log("outside", dog)
-					dog.value = true
-					async.series([
-						function (callback) {
-							dogsBackend.set(dog, callback)
-						},
-						function (callback) {
-							dogsBackend.get()
-						},
-					])
-				},
-				inside: function (dog) {
-					console.log("inside", dog)
-					dog.value = false
-					async.series([
-						function (callback) {
-							dogsBackend.set(dog, callback)
-						},
-						function (callback) {
-							dogsBackend.get()
-						},
-					])
-				},
-				update: function (dogs) {
-					for (dog of dogs) {
-						for (doggie of this.dogs) {
-							if (dog.name === doggie.name && dog.value != doggie.value) {
-								doggie.value = dog.value;
+				methods: {
+					initial: function (dogs) {
+						this.dogs = dogs
+					},
+					outside: function (dog) {
+						console.log("outside", dog)
+						dog.value = true
+						async.series([
+							function (callback) {
+								dogsBackend.set(dog, callback)
+							},
+							function (callback) {
+								dogsBackend.get()
+							},
+						])
+					},
+					inside: function (dog) {
+						console.log("inside", dog)
+						dog.value = false
+						async.series([
+							function (callback) {
+								dogsBackend.set(dog, callback)
+							},
+							function (callback) {
+								dogsBackend.get()
+							},
+						])
+					},
+					update: function (dogs) {
+						for (dog of dogs) {
+							for (doggie of this.dogs) {
+								if (dog.name === doggie.name && dog.value != doggie.value) {
+									doggie.value = dog.value;
+								}
 							}
 						}
-					}
-				},
-			}
+					},
+				}
+			})
+			
+			const vm = app.mount('.dogs')
+			
+			console.log("interval: ", setInterval(function() {
+				dogsBackend.get()
+			}, 5000))
 		})
-		
-		const vm = app.mount('.dogs')
-		
-		console.log("interval: ", setInterval(function() {
-			dogsBackend.get()
-		}, 5000))
 	} else {
 		auth0.loginWithRedirect();
 	}
